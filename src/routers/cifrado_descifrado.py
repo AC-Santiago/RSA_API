568ceb57bb75a04048e9188a8761e9c8e5c43f42
AC-Santiago <acostasantiago311124578@gmail.com>
2024-12-01 13:36:48 -0500

    Se agrego un nuevo endpoint para el cifrado de archivos .txt

--- src/routers/cifrado_descifrado.py
+++ src/routers/cifrado_descifrado.py
@@ -1,9 +1,13 @@
+from concurrent.futures import ThreadPoolExecutor
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, File, Form, UploadFile, status
 from fastapi.responses import JSONResponse
 
-from src.models.cifrado_descifrado_request import CifrarRequest, DescifrarRequest
+from src.models.cifrado_descifrado_request import (
+    CifrarRequest,
+    DescifrarRequest,
+)
 from src.utils.auth import get_current_user
 from src.utils.RSA import RSA
 
@@ -38,5 +42,45 @@ def generar_llaves(user: Annotated[dict, Depends(get_current_user)]):
     llaves: tuple = rsa.generar_claves()
     return JSONResponse(
         content={"llave_publica": llaves[0], "llave_privada": llaves[1]},
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@router.post("/cifrar/file/", tags=["RSA"])
+async def cifrar_file(
+    user: Annotated[dict, Depends(get_current_user)],
+    archivo: Annotated[UploadFile, File(...)],
+    llave_publica: str = Form(...),
+):
+    try:
+        llave_publica = [int(i) for i in llave_publica.split(",")]
+    except ValueError:
+        return JSONResponse(  # noqa
+            content={"detail": "La llave publica debe ser una lista de enteros"},
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if archivo.content_type != "text/plain":
+        return JSONResponse(
+            content={"detail": "El archivo debe ser de tipo texto plano"},
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+    contenido_byte: bytes = await archivo.read()
+    size_chunk = 1024
+    chunks = [
+        contenido_byte[i : i + size_chunk]
+        for i in range(0, len(contenido_byte), size_chunk)
+    ]
+
+    rsa = RSA()
+
+    def cifrar_chunck(chunk: bytes):
+        return rsa.cifrar(chunk.decode("utf-8"), llave_publica)
+
+    with ThreadPoolExecutor() as executor:
+        contenido_byte = "".join(executor.map(cifrar_chunck, chunks))
+
+    return JSONResponse(
+        content={"mensaje_cifrado": f"{contenido_byte}"},
         status_code=status.HTTP_200_OK,
     )
